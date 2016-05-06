56cea566493e6cd2473e78716a312fe1f3d19db9
Jon Clerck <j.m.clerck@icloud.com>
2016-05-06 17:12:48 +0100

    Update dotfile script for new ZSH files

--- dotfile.sh
+++ dotfile.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-if [ `uname` == 'Darwin' ] then
-  # brew  
+if [ `uname` == 'Darwin' ] 
+then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  
-  brew install android-sdk cmake fasd imagemagick wget
-else if [ `uname` == 'Linux ] then
-  # apt-get
+  brew install fasd wget
+else if [ `uname` == 'Linux' ] 
+then
   apt-get install git make
+  git clone https://github.com/clvv/fasd.git
+  cd fasd
+  make install
+fi
 fi
 
 # Nano
 cp .nanorc ~/
 
-# ZSH + Prezto 
-zsh
-
+# Prezto 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 setopt EXTENDED_GLOB
@@ -23,11 +24,6 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
+cp zpreztorc zprofile zshrc ~/.zprezto/runcoms/
+
 chsh -s /bin/zsh
-
-cp .zpreztorc .zprofile .zshrc ~/.zprezto/runcoms
-
-# fasd
-git clone https://github.com/clvv/fasd.git
-cd fasd
-make install
