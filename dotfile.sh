f7e534ddd0355c6af4c4f8960f2c1244bc6ed8ce
Jonathan Clerck <j.m.clerck@icloud.com>
2016-05-23 16:46:35 +0100

    Swap atom for VS Code

--- dotfile.sh
+++ dotfile.sh
@@ -5,17 +5,26 @@ then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew install fasd wget
 
-  # Atom
-  cp -rf .atom ~/
-
   # iTerm
   cp -rf .iterm ~/
+
+  # VS Code  
+  cp -rf .vscode ~/
+
+  ## Link local settings.json to correct location
+  ln -s ~/.vscode/settings.json ~'/Library/Application Support/Code/User/settings.json'
 else if [ `uname` == 'Linux' ] 
 then
   apt-get install git make
   git clone https://github.com/clvv/fasd.git
   cd fasd
   make install
+
+  # VS Code
+  cp -rf .vscode $HOME/
+
+  ## Link local settings.json to correct location
+  ln -s $HOME/.vscode/settings.json $HOME/.config/Code/User/settings.json
 fi
 fi
 
@@ -29,7 +38,7 @@ rm -rf fonts
 # Nano
 cp .nanorc ~/
 
-# Prezto 
+# Prezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 shopt -s extglob
@@ -41,4 +50,4 @@ cp zpreztorc zprofile zshrc ~/.zprezto/runcoms/
 
 chsh -s /bin/zsh
 
-zsh
+exit
