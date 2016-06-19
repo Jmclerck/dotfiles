190ce124ee2a92eeee3ffc90785dc0ae9f236422
Jonathan Clerck <j.m.clerck@icloud.com>
2016-06-19 21:13:16 +0100

    Update dotfile.sh
    Changed Prezto config to Oh-My-ZSH
    Changed powerline fonts to awesome-terminal fonts patched
    Added Fira Mono

--- dotfile.sh
+++ dotfile.sh
@@ -1,52 +1,33 @@
 #!/bin/sh
 
-if [ `uname` == 'Darwin' ] 
-then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew install fasd wget
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install fasd wget
 
-  # iTerm
-  cp -rf .iterm ~/
+# iTerm
+cp -rf .iterm ~/
 
-  # VS Code  
-  cp -rf .vscode ~/
-
-  ## Link local settings.json to correct location
-  ln -s ~/.vscode/settings.json ~'/Library/Application Support/Code/User/settings.json'
-else if [ `uname` == 'Linux' ] 
-then
-  apt-get install git make
-  git clone https://github.com/clvv/fasd.git
-  cd fasd
-  make install
-
-  # VS Code
-  cp -rf .vscode $HOME/
-
-  ## Link local settings.json to correct location
-  ln -s $HOME/.vscode/settings.json $HOME/.config/Code/User/settings.json
-fi
-fi
-
-# Powerline fonts
-git clone https://github.com/powerline/fonts.git
-pushd fonts
-./install.sh
+# Awesome Terminal Fonts Patched
+git clone https://github.com/gabrielelana/awesome-terminal-fonts.git
+pushd awesome-terminal-fonts/patched
+cp *.ttf ~/Library/Fonts
 popd
-rm -rf fonts
+rm -rf awesome-terminal-fonts
+
+# Fira Mono
+git clone https://github.com/mozilla/Fira.git
+pushd Fira-master/ttf
+cp *.ttf ~/Library/Fonts
+popd
+rm -rf Fira-master
 
 # Nano
 cp .nanorc ~/
 
-# Prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+# Oh-My-ZSH
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-shopt -s extglob
-for f in ~/.zprezto/runcoms/!(README.md); do 
-  ln -s $f $HOME/.$(basename $f)
-done
-
-cp zpreztorc zprofile zshrc ~/.zprezto/runcoms/
+cp zshrc ~/.zshrc
+cp powerlevelrc ~/.powerlevelrc
 
 chsh -s /bin/zsh
 
