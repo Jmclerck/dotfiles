f67a823323832850106a81f4b8640f0e829a54c0
Jonathan Clerck <j.m.clerck@icloud.com>
2016-08-01 19:49:12 +0100

    Update dotfile.sh

--- dotfile.sh
+++ dotfile.sh
@@ -16,6 +16,9 @@ pip install glances
 # oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+# powerlevel9k theme
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
 # Hack fonts
 wget https://github.com/chrissimpkins/Hack/releases/download/v2.020/Hack-v2_020-ttf.zip -O hack.zip
 unzip hack.zip
