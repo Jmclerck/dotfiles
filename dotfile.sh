d86ee1e42c339cf6f55d4670149aed12ee72c563
Jon Clerck <j.m.clerck@icloud.com>
2016-11-10 14:19:24 +0000

    Re-arranged symlinks Added symlink for VS Code settings

--- dotfile.sh
+++ dotfile.sh
@@ -1,33 +1,36 @@
 #!/bin/sh
 
-# Symlink Terminal Preferences
-rm ~/Library/Preferences/com.apple.Terminal.plist
-ln -s "$(pwd)/com.apple.Terminal.plist ~/Library/Preferences/com.apple.Terminal.plist
-
-# Symlink RC files
-ln -s "$(pwd)/.nanorc" ~/.nanorc
-ln -s "$(pwd)/.zshrc" ~/.zshrc
-
 # brew utilities
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install cowsay fasd fortune node python tailor wget
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install cowsay fasd fortune node python tailor wget
 
 # brew app casks
-brew cask install docker google-chrome kaleidoscope parallels-desktop paw spectacle tower visual-studio-code
+    brew cask install docker google-chrome kaleidoscope parallels-desktop paw spectacle tower visual-studio-code
 
 # glances
-pip install lolcat
+    pip install lolcat
 
 # oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # gitster theme
-git clone https://github.com/Ranndom/gitster-theme.git ~/.oh-my-zsh/custom/themes/gitster.zsh-theme
+    git clone https://github.com/Ranndom/gitster-theme.git ~/.oh-my-zsh/custom/themes/gitster.zsh-theme
 
 # Hack fonts
-wget https://github.com/chrissimpkins/Hack/releases/download/v2.020/Hack-v2_020-ttf.zip -O hack.zip
-unzip hack.zip
-mv *.ttf ~/Library/Fonts/
-rm hack.zip
+    wget https://github.com/chrissimpkins/Hack/releases/download/v2.020/Hack-v2_020-ttf.zip -O hack.zip
+    unzip hack.zip
+    mv *.ttf ~/Library/Fonts/
+    rm hack.zip
+
+# Symlink Terminal Preferences
+    rm ~/Library/Preferences/com.apple.Terminal.plist
+    ln -s "$(pwd)/com.apple.Terminal.plist" ~/Library/Preferences/com.apple.Terminal.plist
+
+# Sym link VS 
+    ln -s "$(pwd)/settings.json" ~/Library/Application\ Support/Code/User/settings.json
+
+# Symlink RC files
+    ln -s "$(pwd)/.nanorc" ~/.nanorc
+    ln -s "$(pwd)/.zshrc" ~/.zshrc
 
 exit
