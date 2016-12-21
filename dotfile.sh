74ce94dfa30456009aedb8236b70720bf13c9218
Jonathan Clerck <j.m.clerck@icloud.com>
2016-12-21 09:28:26 +0000

    Update dotfile.sh

--- dotfile.sh
+++ dotfile.sh
@@ -1,35 +1,45 @@
 #!/bin/sh
 
+# fake ZSH shell to avoid context switch mid script
+export SHELL="zsh"
+
 # brew utilities
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # brew app casks
-    brew bundle
+brew bundle
 
 # glances
-    pip install lolcat
+pip install lolcat
 
 # trans
-    wget git.io/trans /usr/local/bin/trans
-    chmod +x /usr/local/bin/trans
+wget git.io/trans -O /usr/local/bin/trans
+chmod +x /usr/local/bin/trans
 
 # node apps
-    npm install -g eslint yarn webpack vtop
+npm install -g eslint yarn webpack vtop
 
 # oh-my-zsh
-    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # gitster theme
-    git clone https://github.com/Jmclerck/gitster-theme.git ~/.oh-my-zsh/custom/themes/gitster.zsh-theme
+git clone https://github.com/Jmclerck/gitster-theme.git ~/.oh-my-zsh/custom/themes/gister
+
+# octicons font
+wget https://raw.githubusercontent.com/ryanoasis/nerd-fonts/blob/master/src/glyphs/octicons.ttf -O ~/Library/Fonts/octicons.ttf
 
 # Symlink Terminal Preferences
-    rm ~/Library/Preferences/com.apple.Terminal.plist
-    ln -s "$(pwd)/com.apple.Terminal.plist" ~/Library/Preferences/com.apple.Terminal.plist
+rm ~/Library/Preferences/com.apple.Terminal.plist
+ln -s "$(pwd)/com.apple.Terminal.plist" ~/Library/Preferences/com.apple.Terminal.plist
 
 # Symlink RC files
-    ln -s "$(pwd)/.nanorc" ~/.nanorc
-    ln -s "$(pwd)/zshrc.zsh" ~/.oh-my-zsh/custom/zshrc.zsh
+rm ~/.nanorc
+ln -s "$(pwd)/.nanorc" ~/.nanorc
+rm ~/.oh-my-zsh/custom/zshrc.zsh
+ln -s "$(pwd)/zshrc.zsh" ~/.oh-my-zsh/custom/zshrc.zsh
 
+defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock
 
-reset-launchpad
+chsh -s /bin/zsh
+
 exit
