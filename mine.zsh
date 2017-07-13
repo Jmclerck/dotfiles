a5bfd8dba77ec984d5df474fcedf90ecfa388db2
Jon Clerck <j.m.clerck@icloud.com>
2017-07-13 08:44:08 +0100

    Change functions to aliases

--- mine.zsh
+++ mine.zsh
@@ -7,13 +7,9 @@ ZSH_THEME="zeit"
 
 plugins=(brew docker docker-compose git thefuck node npm osx z)
 
-function reset-dock() {
-	defaults delete com.apple.dock; killall Dock
-}
-
-function reset-launchpad() {
-	defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock
-}
+alias reset-dock="defaults delete com.apple.dock; killall Dock"
+alias reset-launchpad="defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock"
+alias vtop="vtop --theme acid"
 
 eval $(thefuck --alias)
 eval $(ssh-add -K ~/.ssh/id_rsa 2>/dev/null)
