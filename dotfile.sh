40e316cc4730a8c37e23df7871486b7e6889cf1a
Jon Clerck <j.m.clerck@icloud.com>
2017-11-23 06:50:31 +0000

    Trick zsh, remove node apps, install vscode exts

--- dotfile.sh
+++ dotfile.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
+# Set shell to `zsh` 
+export SHELL=/bin/zsh
+
 # brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # brew app casks
 brew bundle
-
-# node apps
-npm install -g vtop
 
 # zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
@@ -31,8 +31,10 @@ ln -s "$(PWD)/code/settings.json" ~/Library/Application\ Support/Code\ -\ Inside
 rm -rf ~/Library/Application\ Support/Code\ -\ Insiders/User/snippets
 ln -s "$(PWD)/code/snippets" ~/Library/Application\ Support/Code\ -\ Insiders/User/snippets
 
-
 # execute reset-launchpad
 /bin/zsh -c 'source ~/.zshrc; reset-launchpad'
+
+# install vscode extensions
+/bin/zsh -c 'for i in `cat vscode.ext`; do; code --install-extension $i; done'
 
 exit
