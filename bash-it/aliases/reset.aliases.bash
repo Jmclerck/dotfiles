2e3ffc7083583d991333b3e70b9beb6d1e53ed49
Jon Clerck <j.m.clerck@icloud.com>
2018-10-22 13:41:30 +0100

    Remove reset aliases
    Signed-off-by: Jon Clerck <j.m.clerck@icloud.com>

--- bash-it/aliases/reset.aliases.bash
+++
@@ -1,7 +0,0 @@
-function reset-dock() {
-  defaults delete com.apple.dock; killall Dock
-}
-
-function reset-launchpad() {
-  defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock
-}
