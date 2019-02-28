c183223acbf31707acc666e26bf675acf0fe5b14
Jon Clerck <j.m.clerck@icloud.com>
2019-02-28 13:29:34 +0000

    Change install to use list of bash_it plugins

--- dotfile.sh
+++ dotfile.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -15,21 +15,19 @@ if [[ ! -a ~/.bash_it ]]; then
   ~/.bash_it/install.sh --no-modify-config --silent
 fi
 
-# Symlink Bash/Bash-IT configuration
-rm ~/.bash_profile
-ln -s "$(PWD)/.bash_profile" ~
-
-# Symlink nano configuration
-rm ~/.nanorc
-ln -s "$(PWD)/.nanorc" ~
-
-# Symlink Visual Studio Code settings
-rm ~/Library/Application\ Support/Code/User/settings.json
-ln -s "$(PWD)/code/settings.json" ~/Library/Application\ Support/Code/User/settings.json
+source "$BASH_IT/bash_it.sh"
 
 # Copy WebIcons font (can't be symlinked, since macOS 10.13)
 rm ~/Library/Fonts/webicons.ttf
 ln -s "$(PWD)/bash-it/themes/font/webicons.ttf" ~/Library/Fonts/
+
+# Symlink Bash/Bash-IT configuration
+rm ~/.bash_profile
+ln -s "$(PWD)/.bash_profile" ~
+
+# Symlink Visual Studio Code settings
+rm ~/Library/Application\ Support/Code/User/settings.json
+ln -s "$(PWD)/code/settings.json" ~/Library/Application\ Support/Code/User/settings.json
 
 # Symlink Visual Studio Code snippets
 rm -rf ~/Library/Application\ Support/Code/User/snippets
@@ -39,24 +37,27 @@ ln -s "$(PWD)/code/snippets" ~/Library/Application\ Support/Code/User/snippets
 rm ~/.hyper.js
 ln -s "$(PWD)/.hyper.js" ~/.hyper.js
 
-# Copy enabled bash-it plugins (can't be symlinked)
-rm -rf ~/.bash_it/enabled
-cp -r "$(PWD)/bash-it/enabled" ~/.bash_it
+# enable bash-it aliases
+for i in $(cat aliases.bash-it); do
+  bash-it enable alias $i
+done
 
-# Add TouchID as sudo authentication method
-touch ./sudo
-sudo sed $'2i\\\nauth       sufficient     pam_tid.so\n' /etc/pam.d/sudo > ./sudo
-sudo chmod -w ./sudo
-sudo chown root ./sudo
-sudo mv /etc/pam.d/sudo /etc/pam.d/sudo~
-sudo mv ./sudo /etc/pam.d/sudo
+# enable bash-it completions
+for i in $(cat completions.bash-it); do
+  bash-it enable completion $i
+done
+
+# enable bash-it plugins
+for i in $(cat plugins.bash-it); do
+  bash-it enable plugin $i
+done
 
 # remove existing vscode extensions
 for i in $(code --list-extensions | comm -13 $(PWD)/vscode.extensions -); do
   code --uninstall-extension $i
 done
 
-# install expected vscode extensions
+# install vscode extensions
 for i in $(cat vscode.extensions); do
   code --install-extension $i
 done
@@ -65,3 +66,11 @@ done
 for i in $(cat npm.modules); do
   npm i -g $i
 done
+
+# Add TouchID as sudo authentication method
+touch ./sudo
+sudo sed $'2i\\\nauth       sufficient     pam_tid.so\n' /etc/pam.d/sudo > ./sudo
+sudo chmod -w ./sudo
+sudo chown root ./sudo
+sudo mv /etc/pam.d/sudo /etc/pam.d/sudo~
+sudo mv ./sudo /etc/pam.d/sudo
--- undotfile.sh
+++ undotfile.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 brew bundle dump --force
 
@@ -9,7 +9,22 @@ npm ls -g --depth=0 --json=true |
   sed -E 's/[^a-z\-]//g' |
   sed '/^$/d' > npm.modules
 
-cp -r ~/.bash_it/enabled ./bash-it/
+source "$BASH_IT/bash_it.sh"
+
+bash-it show alias |
+  grep -Eo "\S*\s*\[x\]" |
+  grep -o "[a-z\-]*" |
+  sed '/^$/d' > aliases.bash-it
+
+bash-it show completions |
+  grep -Eo "\S*\s*\[x\]" |
+  grep -o "[a-z\-]*" |
+  sed '/^$/d' > completions.bash-it
+
+bash-it show plugins |
+  grep -Eo "\S*\s*\[x\]" |
+  grep -o "[a-z\-]*" |
+  sed '/^$/d' > plugins.bash-it
 
 git add .
 git status
