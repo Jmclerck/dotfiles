68ed5dc4bc95e039d82ee10d506884760bebc54f
Jon Clerck <j.m.clerck@icloud.com>
2019-05-28 20:00:38 +1000

    Add Ruby icon

--- bash-it/themes/webicons/webicons.theme.bash
+++ bash-it/themes/webicons/webicons.theme.bash
@@ -2,7 +2,7 @@
 
 ___power=("" "" "" "" "")
 ___power_colours=("${red}" "${yellow}" "${yellow}" "${yellow}" "${green}")
-___prefix=("" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "")
+___prefix=("" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "")
 ___prefix_colours=("${reset_color}" "${reset_color}" "${reset_color}" "${reset_color}" "${reset_color}" "${reset_color}" "${reset_color}" "${blue}" "${blue}" "${blue}" "${blue}" "${blue}" "${blue}" "${blue}" "${green}" "${green}" "${magenta}" "${orange}" "${orange}" "${orange}" "${orange}" "${purple}" "${purple}" "${purple}" "${purple}" "${red}" "${yellow}" "${yellow}")
 
 ___silent() {
@@ -11,104 +11,108 @@ ___silent() {
 }
 
 __power() {
-  local status=$(pmset -g batt)
+  if [[ $THEME_POWER_MONITOR != false ]]; then
+    local status=$(pmset -g batt)
 
-  local charging=$(echo $status | grep -Eo "\scharg[ing|ed]")
-  local discharging=$(echo $status | grep -o "\sdischarging")
-  local percent=$(echo $status | grep -o "[0-9]*%" | grep -o "[0-9]*")
-  local segment=$(( percent / 25 ))
+    local charging=$(echo $status | grep -Eo "\scharg[ing|ed]")
+    local discharging=$(echo $status | grep -o "\sdischarging")
+    local percent=$(echo $status | grep -o "[0-9]*%" | grep -o "[0-9]*")
+    local segment=$(( percent / 25 ))
 
-  local colour=${___power_colours[$segment]}
+    local colour=${___power_colours[$segment]}
 
-  if [[ -n $charging ]]; then
-    local time=$(echo $status | grep -o "[0-9]:[0-9]*")
+    if [[ -n $charging ]]; then
+      local time=$(echo $status | grep -o "[0-9]:[0-9]*")
 
-    echo "${colour}$percent%  ${reset_color} "
-  elif [[ -n $discharging ]]; then
-    echo "${colour}$percent% ${___power[$segment]} ${reset_color} "
+      echo "${colour}$percent%  ${reset_color} "
+    elif [[ -n $discharging ]]; then
+      echo "${colour}$percent% ${___power[$segment]} ${reset_color} "
+    fi
   fi
 }
 
 __stat() {
-  local icons=''
-  local git=$(git rev-parse --is-inside-work-tree 2> /dev/null)
+  if [[ $THEME_GIT_STATUS != false ]]; then
+    local icons=''
+    local git=$(git rev-parse --is-inside-work-tree 2> /dev/null)
 
-  if [[ $git == true ]]; then
-    local stashes=$(git stash list | grep -o '@' |  tr -d ' ' | tr -d '\n')
-    local numberOfStashes=${#stashes}
-    if [[ $numberOfStashes -gt 0 ]]; then
-      icons="$icons ${magenta} $numberOfStashes${reset_color}"
-    fi
-
-    local untracked=$(git status --porcelain | grep -o '^??\s' |  tr -d ' ' | tr -d '\n')
-    local numberOfUntracked=${#untracked}
-    if [[ $numberOfUntracked -gt 0 ]]; then
-      icons="$icons ${orange} $(($numberOfUntracked / 2))${reset_color}"
-    fi
-
-    local added=$(git status --porcelain | grep -oE '^\sA\s|^A\s{2}' |  tr -d ' ' | tr -d '\n')
-    local numberOfAdded=${#added}
-    if [[ $numberOfAdded -gt 0 ]]; then
-      icons="$icons ${green} $numberOfAdded${reset_color}"
-    fi
-
-    local deleted=$(git status --porcelain | grep -oE '^\sD\s|^D\s{2}' |  tr -d ' ' | tr -d '\n')
-    local numberOfDeleted=${#deleted}
-    if [[ $numberOfDeleted -gt 0 ]]; then
-      icons="$icons ${red} $numberOfDeleted${reset_color}"
-    fi
-
-    local modified=$(git status --porcelain | grep -oE '^\sM\s|^M\s{2}' |  tr -d ' ' | tr -d '\n')
-    local numberOfModified=${#modified}
-    if [[ $numberOfModified -gt 0 ]]; then
-      icons="$icons ${orange} $numberOfModified${reset_color}"
-    fi
-
-    local renamed=$(git status --porcelain | grep -oE '^\sR\s|^R\s{2}' |  tr -d ' ' | tr -d '\n')
-    local numberOfRenamed=${#renamed}
-    if [[ $numberOfRenamed -gt 0 ]]; then
-      icons="$icons ${green} $numberOfRenamed${reset_color}"
-    fi
-
-    local conflicts=$(git status --porcelain | grep -oE '^UU\s' |  tr -d ' ' | tr -d '\n')
-    local numberOfConflicts=${#conflicts}
-    if [[ $numberOfConflicts -gt 0 ]]; then
-      icons="$icons ${red} $(($numberOfConflicts / 2))${reset_color}"
-    fi
-
-    local staged=$(git status --porcelain |  grep -oE '^A\s{2}|^D\s{2}|^M\s{2}|^R\s{2}' | tr -d ' ' | tr -d '\n')
-    local numberOfStaged=${#staged}
-    if [[ $numberOfStaged -gt 0 ]]; then
-      icons="$icons ${green} $(($numberOfStaged))${reset_color}"
-    fi
-
-    local remote=$(git show-ref origin/$(_git-branch) 2> /dev/null)
-    if [[ -z $remote ]]; then
-      icons="$icons ${red}${reset_color}"
-    else
-      local str=$(_git-upstream-behind-ahead)
-      local counts=($(echo "$str" | tr ' ' '\n'))
-
-      local behind=${counts[0]}
-      if [[ $behind -gt 0 ]]; then
-        icons="$icons ${orange} $behind${reset_color}"
+    if [[ $git == true ]]; then
+      local stashes=$(git stash list | grep -o '@' |  tr -d ' ' | tr -d '\n')
+      local numberOfStashes=${#stashes}
+      if [[ $numberOfStashes -gt 0 ]]; then
+        icons="$icons ${magenta} $numberOfStashes${reset_color}"
       fi
 
-      local ahead=${counts[1]}
-      if [[ $ahead -gt 0 ]]; then
-        icons="$icons ${green} $ahead${reset_color}"
+      local untracked=$(git status --porcelain | grep -o '^??\s' |  tr -d ' ' | tr -d '\n')
+      local numberOfUntracked=${#untracked}
+      if [[ $numberOfUntracked -gt 0 ]]; then
+        icons="$icons ${orange} $(($numberOfUntracked / 2))${reset_color}"
       fi
 
-      if [[ $(_git-branch) == "master" ]]; then
-        icons="$icons ${blue}${reset_color}"
+      local added=$(git status --porcelain | grep -oE '^\sA\s|^A\s{2}' |  tr -d ' ' | tr -d '\n')
+      local numberOfAdded=${#added}
+      if [[ $numberOfAdded -gt 0 ]]; then
+        icons="$icons ${green} $numberOfAdded${reset_color}"
+      fi
+
+      local deleted=$(git status --porcelain | grep -oE '^\sD\s|^D\s{2}' |  tr -d ' ' | tr -d '\n')
+      local numberOfDeleted=${#deleted}
+      if [[ $numberOfDeleted -gt 0 ]]; then
+        icons="$icons ${red} $numberOfDeleted${reset_color}"
+      fi
+
+      local modified=$(git status --porcelain | grep -oE '^\sM\s|^M\s{2}' |  tr -d ' ' | tr -d '\n')
+      local numberOfModified=${#modified}
+      if [[ $numberOfModified -gt 0 ]]; then
+        icons="$icons ${orange} $numberOfModified${reset_color}"
+      fi
+
+      local renamed=$(git status --porcelain | grep -oE '^\sR\s|^R\s{2}' |  tr -d ' ' | tr -d '\n')
+      local numberOfRenamed=${#renamed}
+      if [[ $numberOfRenamed -gt 0 ]]; then
+        icons="$icons ${green} $numberOfRenamed${reset_color}"
+      fi
+
+      local conflicts=$(git status --porcelain | grep -oE '^UU\s' |  tr -d ' ' | tr -d '\n')
+      local numberOfConflicts=${#conflicts}
+      if [[ $numberOfConflicts -gt 0 ]]; then
+        icons="$icons ${red} $(($numberOfConflicts / 2))${reset_color}"
+      fi
+
+      local staged=$(git status --porcelain |  grep -oE '^A\s{2}|^D\s{2}|^M\s{2}|^R\s{2}' | tr -d ' ' | tr -d '\n')
+      local numberOfStaged=${#staged}
+      if [[ $numberOfStaged -gt 0 ]]; then
+        icons="$icons ${green} $(($numberOfStaged))${reset_color}"
+      fi
+
+      local remote=$(git show-ref origin/$(_git-branch) 2> /dev/null)
+      if [[ -z $remote ]]; then
+        icons="$icons ${red}${reset_color}"
       else
-        icons="$icons ${blue}${reset_color}"
-      fi
-    fi
+        local str=$(_git-upstream-behind-ahead)
+        local counts=($(echo "$str" | tr ' ' '\n'))
 
-    printf " at ${magenta}$(_git-branch)$icons"
-  else
-    printf "  "
+        local behind=${counts[0]}
+        if [[ $behind -gt 0 ]]; then
+          icons="$icons ${orange} $behind${reset_color}"
+        fi
+
+        local ahead=${counts[1]}
+        if [[ $ahead -gt 0 ]]; then
+          icons="$icons ${green} $ahead${reset_color}"
+        fi
+
+        if [[ $(_git-branch) == "master" ]]; then
+          icons="$icons ${blue}${reset_color}"
+        else
+          icons="$icons ${blue}${reset_color}"
+        fi
+      fi
+
+      printf " at ${magenta}$(_git-branch)$icons"
+    else
+      printf "  "
+    fi
   fi
 }
 
@@ -147,8 +151,13 @@ __updates() {
     fi
   }
 
-  ___silent ___homebrew_updates
-  ___silent ___npm_updates
+  if [[ $THEME_BREW_UPDATE != false ]]; then
+    ___silent ___homebrew_updates
+  fi
+
+  if [[ $THEME_NPM_UPDATE != false ]]; then
+    ___silent ___npm_updates
+  fi
 }
 
 __versions() {
@@ -160,6 +169,10 @@ __versions() {
 
   if [[ $THEME_NODE_VERSION != false ]]; then
     icons="$icons${green}  $(npm config get node-version)"
+  fi
+
+  if [[ $THEME_RUBY_VERSION != false ]]; then
+    icons="$icons${red}  $(ruby --version | grep -o "\d*\.\d*\.\d*")"
   fi
 
   printf "$icons"
