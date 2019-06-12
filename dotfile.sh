fea19ba91de1a7e814abcc55261712e2b0e12fb3
Jon Clerck <j.m.clerck@icloud.com>
2019-06-12 08:58:05 +0100

    Add ZSH config

--- dotfile.sh
+++ dotfile.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# silence last login message
+touch .hushlogin
+
+# zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh --unattended)"
+
 # brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
