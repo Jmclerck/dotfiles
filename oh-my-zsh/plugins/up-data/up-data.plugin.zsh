d1379b308eff8fe92929ce7e7275ae26055debfc
Jon Clerck <j.m.clerck@icloud.com>
2019-06-14 15:07:56 +0100

    Fix missing update notifications

--- oh-my-zsh/plugins/up-data/up-data.plugin.zsh
+++ oh-my-zsh/plugins/up-data/up-data.plugin.zsh
@@ -13,29 +13,54 @@ function __updates() {
 
     local casks=$(brew cask outdated 2> /dev/null | wc -l | tr -d ' ')
     if [[ $casks -eq 1 ]]; then
-      echo "$casks homebrew cask update" | terminal-notifier -activate $UP_DATA_TERMINAL_APP -appIcon $ZSH_CUSTOM/plugins/up-data/icons/homebrew.png -sound default -title 'webicons'
+      echo "$casks homebrew cask update" | terminal-notifier \
+        -activate $UP_DATA_TERMINAL_APP \
+        -appIcon $ZSH_CUSTOM/plugins/up-data/icons/homebrew.png \
+        -sound submarine \
+        -title 'webicons'
     elif [[ $casks -ge 2 ]]; then
-      echo "$casks homebrew cask updates" | terminal-notifier -activate $UP_DATA_TERMINAL_APP -appIcon $ZSH_CUSTOM/plugins/up-data/icons/homebrew.png -sound default -title 'webicons'
+      echo "$casks homebrew cask updates" | terminal-notifier \
+        -activate $UP_DATA_TERMINAL_APP \
+        -appIcon $ZSH_CUSTOM/plugins/up-data/icons/homebrew.png \
+        -sound submarine \
+        -title 'webicons'
     fi
 
     local brews=$(brew outdated 2> /dev/null | wc -l | tr -d ' ')
     if [[ $brews -eq 1 ]]; then
-      echo "$brews homebrew update" | terminal-notifier -activate $UP_DATA_TERMINAL_APP -appIcon $ZSH_CUSTOM/plugins/up-data/icons/homebrew.png -sound default -title 'webicons'
+      echo "$brews homebrew update" | terminal-notifier
+        -activate $UP_DATA_TERMINAL_APP \
+        -appIcon $ZSH_CUSTOM/plugins/up-data/icons/homebrew.png \
+        -sound submarine \
+        -title 'webicons'
     elif [[ $brews -ge 2 ]]; then
-      echo "$brews homebrew updates" | terminal-notifier -activate $UP_DATA_TERMINAL_APP -appIcon $ZSH_CUSTOM/plugins/up-data/icons/homebrew.png -sound default -title 'webicons'
+      echo "$brews homebrew updates" | terminal-notifier \
+        -activate $UP_DATA_TERMINAL_APP \
+        -appIcon $ZSH_CUSTOM/plugins/up-data/icons/homebrew.png \
+        -sound submarine \
+        -title 'webicons'
     fi
   fi
 
   if [[ $THEME_NPM_UPDATE != false ]]; then
     local npm=$(npm outdated -g --json=true | jq 'keys' | sed -E 's/[^a-z\-]//g' | sed '/^$/d' 2> /dev/null | wc -l | tr -d ' ')
     if [[ $npm -eq 1 ]]; then
-      echo "$npm npm update" | terminal-notifier -activate $UP_DATA_TERMINAL_APP -appIcon $ZSH_CUSTOM/plugins/up-data/icons/npm.png -sound default -title 'webicons'
+      echo "$npm npm update" | terminal-notifier \
+        -activate $UP_DATA_TERMINAL_APP \
+        -appIcon $ZSH_CUSTOM/plugins/up-data/icons/npm.png \
+        -sound submarine \
+        -title 'webicons'
     elif [[ $npm -ge 2 ]]; then
-      echo "$npm npm updates" | terminal-notifier -activate $UP_DATA_TERMINAL_APP -appIcon $ZSH_CUSTOM/plugins/up-data/icons/npm.png -sound default -title 'webicons'
+      echo "$npm npm updates" | terminal-notifier \
+        -activate $UP_DATA_TERMINAL_APP \
+        -appIcon $ZSH_CUSTOM/plugins/up-data/icons/npm.png \
+        -sound submarine \
+        -title 'webicons'
     fi
   fi
 }
 
 async_init
-async_start_worker my_worker
-async_job my_worker __updates
+async_start_worker up_data_worker
+async_flush_jobs up_data_worker
+async_job up_data_worker __updates
