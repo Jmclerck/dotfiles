b0adaf28cac34ffd3b1d61d690935141f350c693
Jon Clerck <j.m.clerck@icloud.com>
2019-11-06 10:56:43 +0000

    remove nvs

--- dotfile.sh
+++ dotfile.sh
@@ -1,18 +1,14 @@
 # zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh --unattended)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
 # zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-
-# nvs
-git clone https://github.com/jasongin/nvs "$HOME/.nvs"
-. "$HOME/.nvs/nvs.sh" install
 
 # yarn
 curl -o- -L https://yarnpkg.com/install.sh | bash
 
 # brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # remove brew apps
 brew bundle cleanup --force
