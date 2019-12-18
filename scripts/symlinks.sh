cfbafb476c707264d8356c9180b11163ab5d6ef0
Jon Clerck <j.m.clerck@icloud.com>
2019-12-18 12:30:28 +0000

    fix font install

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,15 +1,15 @@
-# Copy WebIcons font (can't be symlinked, since macOS 10.13)
-rm ~/Library/Fonts/webicons.ttf 2&> /dev/null
-ln -s "$(pwd)/bash-it/themes/font/webicons.ttf" ~/Library/Fonts/
-
 # Symlink zshrc configuration
 rm ~/.zshrc 2&> /dev/null
 ln -s "$(pwd)/.zshrc" ~
 
-# Symlink Visual Studio Code settings
+# Symlink vscode settings
 rm ~/Library/Application\ Support/Code/User/settings.json 2&> /dev/null
 ln -s "$(pwd)/code/settings.json" ~/Library/Application\ Support/Code/User/settings.json
 
-# Symlink Visual Studio Code snippets
+# Symlink vscode snippets
 rm -rf ~/Library/Application\ Support/Code/User/snippets 2&> /dev/null
 ln -s "$(pwd)/code/snippets" ~/Library/Application\ Support/Code/User/snippets
+
+# Copy webicons font
+rm ~/Library/Fonts/webicons.ttf 2&> /dev/null
+cp "$(pwd)/oh-my-zsh/themes/webicons/font/webicons.ttf" ~/Library/Fonts/
