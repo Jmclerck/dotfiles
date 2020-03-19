2e40faab308831201dea338d7c66e6dd2548e3d2
Jon Clerck <j.m.clerck@icloud.com>
2020-03-19 10:31:30 +0000

    Remove unnecessary file removal
    Signed-off-by: Jon Clerck <j.m.clerck@icloud.com>

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -3,8 +3,9 @@ rm ~/.zshrc 2&> /dev/null
 ln -s "$(pwd)/.zshrc" ~
 
 # Copy webicons font
-rm ~/Library/Fonts/webicons.ttf 2&> /dev/null
-cp "$(pwd)/oh-my-zsh/themes/webicons/font/webicons.ttf" ~/Library/Fonts/
 
-rm ~/Library/Application\ Support/iTerm2/Scripts/AutoLaunch 2&> /dev/null
-ln -s "$(pwd)/iTerm2/AutoLaunch" ~/Library/Application\ Support/iTerm2/Scripts/
+ln -sf "$(pwd)/.zshrc" ~
+
+ln -sf "$(pwd)/iTerm2/AutoLaunch" ~/Library/Application\ Support/iTerm2/Scripts/
+
+cp "$(pwd)/oh-my-zsh/themes/webicons/font/webicons.ttf" ~/Library/Fonts/
