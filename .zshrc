e49243703cd3e94df5256bfaaa2760783102a36b
Jon Clerck <j.m.clerck@icloud.com>
2020-05-26 09:27:10 +0100

    Remove yarn install script
    Signed-off-by: Jon Clerck <j.m.clerck@icloud.com>

--- .zshrc
+++ .zshrc
@@ -1,5 +1,5 @@
 # If you come from bash you might have to change your $PATH.
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$HOME/.cargo/bin:/usr/local/sbin:$PATH"
+export PATH="$HOME/.cargo/bin:/usr/local/sbin:$PATH"
 
 # Path to your oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
--- dotfile.sh
+++ dotfile.sh
@@ -1,9 +1,6 @@
 # zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
-# yarn
-curl -o- -L https://yarnpkg.com/install.sh | bash
-
 # brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew bundle cleanup --force
