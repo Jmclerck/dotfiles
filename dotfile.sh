7974d370158acdb5b982953a5dbe714bd69e0051
Jon Clerck <j.m.clerck@icloud.com>
2020-06-28 21:24:03 +0100

    Add async git status to prompt
    Signed-off-by: Jon Clerck <j.m.clerck@icloud.com>

--- dotfile.sh
+++ dotfile.sh
@@ -6,8 +6,10 @@
 # shell
     ln -sf "$(pwd)/.zshrc" ~
 
+    git clone git@github.com:Jmclerck/zsh-git-status.git ~/.zsh/zsh-git-status
     git clone git@github.com:Jmclerck/zsh-versions.git ~/.zsh/zsh-versions
     git clone git@github.com:Jmclerck/zsh-webicons.git ~/.zsh/zsh-webicons
+    git clone git@github.com:mafredri/zsh-async.git ~/.zsh/zsh-async
     git clone git@github.com:zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
     git clone git@github.com:zsh-users/zsh-completions.git ~/.zsh/zsh-completions
     git clone git@github.com:zsh-users/zsh-history-substring-search.git ~/.zsh/zsh-history-substring-search
