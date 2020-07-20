87116fd084fce030f494a507c3ea834480b65398
Jon Clerck <j.m.clerck@icloud.com>
2020-07-20 23:13:25 +0100

    Add wd to install list

--- dotfile.sh
+++ dotfile.sh
@@ -13,10 +13,11 @@
     git clone git@github.com:Jmclerck/zsh-versions.git ~/.zsh/zsh-versions
     git clone git@github.com:Jmclerck/zsh-webicons.git ~/.zsh/zsh-webicons
     git clone git@github.com:mafredri/zsh-async.git ~/.zsh/zsh-async
+    git clone git@github.com:mfaerevaag/wd.git ~/.zsh/wd
+    git clone git@github.com:zdharma/fast-syntax-highlighting ~/.zsh/fast-syntax-highlighting
     git clone git@github.com:zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
     git clone git@github.com:zsh-users/zsh-completions.git ~/.zsh/zsh-completions
     git clone git@github.com:zsh-users/zsh-history-substring-search.git ~/.zsh/zsh-history-substring-search
-    git clone git@github.com:zdharma/fast-syntax-highlighting ~/.zsh/fast-syntax-highlighting
 
 # preferences
     # Close any open System Preferences panes, to prevent them from overriding
