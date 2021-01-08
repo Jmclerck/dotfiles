69d5261307ad5a3f745a865513bcdf3c73db1038
Jon Clerck <j.m.clerck@icloud.com>
2021-01-08 10:44:31 +0000

    Add non-Brew applications

--- .scripts/applications.sh
+++ .scripts/applications.sh
@@ -1,8 +1,20 @@
-open https://github.com/apple/swift-format
+open https://brew.sh
+
 open https://github.com/BurntSushi/ripgrep
 open https://github.com/cli/cli
 open https://github.com/github/hub
 open https://github.com/schniz/fnm
 open https://deno.land/
+
+open https://backblaze.com/
+open https://git-tower.com/
 open https://gnupg.org/
-open https://brew.sh
+open https://malwarebytes.com/
+open https://obdev.at/products/littlesnitch/
+open https://parallels.com/
+open https://paw.cloud/
+open https://rectangleapp.com/
+open https://reincubate.com/camo/
+open https://sensei.app/
+open https://telestream.net/screenflow/
+open https://visualstudio.microsoft.com/
