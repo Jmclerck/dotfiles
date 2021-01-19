d3ec1c8b67ae5ef05a086630b95d1ee7b3ae4e5a
Jonathan Clerck <j.m.clerck@icloud.com>
2021-01-19 11:26:38 +0000

    Re-order application

--- .scripts/applications.sh
+++ .scripts/applications.sh
@@ -7,14 +7,15 @@ open https://github.com/schniz/fnm
 open https://deno.land/
 
 open https://backblaze.com/
-open https://git-tower.com/
 open https://gnupg.org/
 open https://malwarebytes.com/
 open https://obdev.at/products/littlesnitch/
-open https://parallels.com/
 open https://paw.cloud/
 open https://rectangleapp.com/
 open https://reincubate.com/camo/
 open https://sensei.app/
+
+open https://git-tower.com/
+open https://parallels.com/
 open https://telestream.net/screenflow/
 open https://visualstudio.microsoft.com/
