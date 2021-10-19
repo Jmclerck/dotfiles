9e307dcbb95cd3fb8ecc97f60fb930450f144f16
Jonathan Clerck <j.m.clerck@icloud.com>
2021-10-19 12:37:16 +0100

    Remove PG status

--- .scripts/shell.sh
+++ .scripts/shell.sh
@@ -4,7 +4,6 @@ git clone git@github.com:Jmclerck/zsh-battery-status.git ~/.zsh/zsh-battery-stat
 git clone git@github.com:Jmclerck/zsh-ci-status.git ~/.zsh/zsh-ci-status
 git clone git@github.com:Jmclerck/zsh-command-status.git ~/.zsh/zsh-command-status
 git clone git@github.com:Jmclerck/zsh-git-status.git ~/.zsh/zsh-git-status
-git clone git@github.com:Jmclerck/zsh-pg-status.git ~/.zsh/zsh-pg-status
 git clone git@github.com:Jmclerck/zsh-versions.git ~/.zsh/zsh-versions
 git clone git@github.com:Jmclerck/zsh-webicons.git ~/.zsh/zsh-webicons
 git clone git@github.com:mafredri/zsh-async.git ~/.zsh/zsh-async
