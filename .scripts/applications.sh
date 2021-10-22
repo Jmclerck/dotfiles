33a8dff48a4a1c33567136956c4ce2290716ee3e
Jonathan Clerck <j.m.clerck@icloud.com>
2021-10-22 17:08:32 +0100

    Cleanup applications list

--- .scripts/applications.sh
+++ .scripts/applications.sh
@@ -1,12 +1,6 @@
-open https://brew.sh
-open https://github.com/BurntSushi/ripgrep
-open https://github.com/cli/cli
-open https://github.com/schniz/fnm
-
+open https://cli.github.com/
 open https://gnupg.org/
 open https://malwarebytes.com/
 open https://obdev.at/products/littlesnitch/
 open https://reincubate.com/camo/
-open https://sensei.app/
-open https://telestream.net/screenflow/
 open https://visualstudio.microsoft.com/
