71f403e03e6dcd5aa1930029414b93d47b27be77
Jonathan Clerck <jonathan@edil.is>
2021-11-02 15:39:00 +0000

    Remove fast-syntax-highlighting

--- .zshrc
+++ .zshrc
@@ -6,7 +6,7 @@ source ~/.zsh/zsh-battery-status/zsh-battery-status.zsh
 source ~/.zsh/zsh-completions/zsh-completions.plugin.zsh
 source ~/.zsh/zsh-git-status/zsh-git-status.zsh
 source ~/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh
-source ~/.zsh/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
 source ~/.zprofile
 
 autoload -Uz colors
--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@ git clone git@github.com:Jmclerck/zsh-battery-status.git ~/.zsh/zsh-battery-stat
 git clone git@github.com:Jmclerck/zsh-git-status.git ~/.zsh/zsh-git-status
 git clone git@github.com:mafredri/zsh-async.git ~/.zsh/zsh-async
 git clone git@github.com:mfaerevaag/wd.git ~/.zsh/wd
-git clone git@github.com:zdharma/fast-syntax-highlighting ~/.zsh/fast-syntax-highlighting
+git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 git clone git@github.com:zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
 git clone git@github.com:zsh-users/zsh-completions.git ~/.zsh/zsh-completions
 git clone git@github.com:zsh-users/zsh-history-substring-search.git ~/.zsh/zsh-history-substring-search
+
+mkdir ~/.zsh/completions
