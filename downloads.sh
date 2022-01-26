71ae104bb2bd4ad5b0d85822c99901a1aac940fb
Jonathan Clerck <j.m.clerck@icloud.com>
2022-01-26 21:05:28 +0000

    Remove no longer used apps

--- downloads.sh
+++ downloads.sh
@@ -1,10 +1,6 @@
 open https://www.jetbrains.com/lp/mono/
 
-open https://cli.github.com/
 open https://desktop.github.com/
 open https://gnupg.org/
-open https://malwarebytes.com/
-open https://obdev.at/products/littlesnitch/
 open https://reincubate.com/camo/
 open https://visualstudio.microsoft.com/
-
