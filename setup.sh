3cc6437e35e5fa3e1c3eb64030eee3dc67b13635
Jonathan Clerck <j.m.clerck@icloud.com>
2022-06-13 14:20:31 +0100

    remove obsolete directory creation

--- setup.sh
+++ setup.sh
@@ -8,5 +8,3 @@ git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax
 git clone git@github.com:zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
 git clone git@github.com:zsh-users/zsh-completions.git ~/.zsh/zsh-completions
 git clone git@github.com:zsh-users/zsh-history-substring-search.git ~/.zsh/zsh-history-substring-search
-
-mkdir ~/.zsh/zsh-user-completions
