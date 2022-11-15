af95803e141e4e79885664dbcac78c3428024157
Jonathan Clerck <j.m.clerck@icloud.com>
2022-11-15 14:44:43 +0000

    Revert "Add Affinity Designer v2 to downloads list"
    This reverts commit ebaa8ded859310feb50e7b1cc138e321edf66b9d.

--- downloads.sh
+++ downloads.sh
@@ -2,4 +2,3 @@ open https://1password.com/downloads/mac/
 open https://desktop.github.com/
 open https://jetbrains.com/toolbox
 open https://www.rectangleapp.com
-open https://affinity.serif.com/en-gb/designer/
