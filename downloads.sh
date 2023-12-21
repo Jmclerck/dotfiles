1d44c9438ec3c77b2eda24fd5f408645062a9fe2
Jon Clerck <jonathan@edil.is>
2023-12-21 17:44:57 +0000

    Add optional develoment downloads

--- downloads.sh
+++ downloads.sh
@@ -1,7 +1,38 @@
-open https://1password.com/downloads/mac/
-open https://code.visualstudio.com
-open https://desktop.github.com/
-open https://paw.cloud
-open https://www.getvivid.app
-open https://www.parallels.com/products/desktop/
-open https://www.rectangleapp.com
+while true; do
+    read -p "Install Applications? " yn
+    case $yn in
+        [Yy]* ) 
+            open https://1password.com/downloads/mac/
+            open https://code.visualstudio.com
+            open https://kaleidoscope.app/download
+            open https://store.serif.com/get/universal-licence-2/trial/
+            open https://www.getvivid.app
+            open https://www.git-tower.com/mac
+            open https://www.parallels.com/products/desktop/
+            open https://www.rectangleapp.com
+            break;;
+        [Nn]* ) 
+            break;;
+        [Ll]* )
+            echo "1password, Visual Studio, Kaleidoscope, Affinity Designer, Vivid, Tower Parallels Desktop, Rectangle";;
+        * ) 
+            echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Install development tools? " yn
+    case $yn in
+        [Yy]* ) 
+            open https://github.com/mklement0/n-install; 
+            open https://docs.deno.com/runtime/manual/getting_started/installation;
+            open https://developer.1password.com/docs/cli/get-started/#step-1-install-1password-cli;
+            break;;
+        [Nn]* )
+            break;;
+        [Ll]* )
+            echo "n-install, deno, 1password CLI";;
+        * ) 
+            echo "Please answer yes or no.";;
+    esac
+done
